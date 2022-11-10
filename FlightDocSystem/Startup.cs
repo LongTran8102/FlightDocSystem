61a729ce9c59cb1ecf78ffe0df82492be93d47af
LongTran8102 <longtpp8102@gmail.com>
2022-11-10 13:53:49 +0700

    first commit

---
+++ FlightDocSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlightDocSystem.Startup))]
+namespace FlightDocSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
